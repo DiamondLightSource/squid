4416d4a39bf764efc121ff3260514e197ff7da16
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-24 14:05:45 +0100

    no pnpmstore

---
+++ turborepo/packages/ui/src/nicecomponent.tsx
@@ -0,0 +1,8 @@
+export const Nicecomponent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Nicecomponent Component</h1>
+      {children}
+    </div>
+  );
+};
