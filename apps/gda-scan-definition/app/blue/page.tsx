1ec26e795eb77e83c302ee3d3c168d5266bf964d
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-02-21 13:55:24 +0000

    hey api test

---
+++ apps/gda-scan-definition/app/blue/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
+import { blueapiClient } from "../clients/blueapi/client.gen";
+import { getPlans } from "../clients/blueapi";
+import { GetPlansPlansGetResponse, PlanResponse } from "../clients/blueapi";
+
+
+export default function Blue() {
+
+    const [plans, setPlans] = useState([]);
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            console.log(`client: ${blueapiClient}`);
+            console.dir(blueapiClient, { depth: null });
+
+            const plans_response: PlanResponse = await blueapiClient.get();
+            const plans = plans_response.plans;
+            console.log(plans);
+            setPlans(plans);
+        };
+
+        fetchData().catch(console.error);
+
+    }, [])
+
+    return <div>
+
+
+        <p>list avialable plans</p>
+        <div>
+            <ul>
+
+                {plans.map((p, i) => {
+                    return <li key={`device-${i}`}>
+                        <p>
+                            {p.name}
+                        </p>
+                        <p>
+                            {p.description}
+                        </p>
+                    </li>
+                })}
+            </ul>
+        </div>
+
+    </div>
+}
