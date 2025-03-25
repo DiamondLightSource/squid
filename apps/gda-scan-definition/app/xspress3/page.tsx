695593435bef93200cd41c1e68e302781dada2e7
Andrea Liao <andrea.liao@diamond.ac.uk>
2025-03-25 12:05:36 +0000

    make zod schemas for xspress3 configuration

---
+++ apps/gda-scan-definition/app/xspress3/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Xspress3ConfigPage() {
+    return (
+        <div>
+            <Typography variant="h2" sx={{color: 'black'}}>xpress3 config here</Typography>    
+        </div>
+    )
+}
+
+export default Xspress3ConfigPage
