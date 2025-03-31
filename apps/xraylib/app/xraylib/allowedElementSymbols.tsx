16ddd374868ba48d1d1f45147d5878fba35da600
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-03-31 11:10:55 +0000

    fix the filter to allow hydrogen

--- apps/xraylib/app/xraylib/allowedElementSymbols.tsx
+++ apps/xraylib/app/xraylib/allowedElementSymbols.tsx
@@ -3,6 +3,6 @@ import { elements } from "@diamondlightsource/periodic-table/elements";
 export const allowedElementSymbols = elements
   .filter((e) => {
     const n = parseInt(e.Number);
-    return 1 < n && n < 109;
+    return 1 <= n && n < 109;
   })
   .map((element) => element.Symbol) as [string, ...string[]];
